1d755b0cc9bb97f7f670b9602e566ac7d59f7ce4
Andrii Lutsiak <timechain04@gmail.com>
2023-07-13 19:50:39 +0300

    pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import FeedbackOptions from "FeedbackOptions/FeedbackOptions";
-import Section from "Section/Section";
-import Notification from "Notification/Notification";
-import Statistics from "Statistics/Statistics";
+import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
+import Section from "./Section/Section";
+import Notification from "./Notification/Notification";
+import Statistics from "./Statistics/Statistics";
 
 class App extends Component {
   state = {
